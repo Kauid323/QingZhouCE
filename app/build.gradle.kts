eacaa626cb5063139b42ce84a12653db4bf49e42
ShiQi-17 <19136769323@163.com>
2026-04-25 21:18:20 +0800

    更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 23
         targetSdk = 36
         versionCode = 1
-        versionName = "0.2.0"
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
